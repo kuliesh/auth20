82cd8f00e5238a150d97ea3756661fe1826ec993
s.kuliesh <s.kuliesh>
2018-06-27 18:37:01 +0300

    added two bases classes for test on Chrome and Firefox

--- src/tests/java/auth/dev/net/alltests/BaseClassesForTests/BaseTestsChrome.java
+++ src/tests/java/auth/dev/net/alltests/BaseClassesForTests/BaseTestsChrome.java
@@ -1,17 +1,17 @@
-package net.ukr.dev.BasesClassesForTests;
+package auth.dev.net.alltests.BaseClassesForTests;
 
 import org.apache.log4j.Logger;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-public class BaseTests {
-    final static Logger logger = Logger.getLogger(BaseTests.class);
+public class BaseTestsChrome {
+    final static Logger logger = Logger.getLogger(BaseTestsChrome.class);
 
     @BeforeTest
     public static void setup() {
         logger.info("Run browser...");
         System.setProperty("selenide.browser", "chrome");
-        System.setProperty("webdriver.chrome.driver", "D:/AutomationTest/authtest");
+        System.setProperty("webdriver.chrome.driver", "D:/AutomationTest/auth20/src/tests/java/auth/dev/net/resources");
     }
 
     @AfterTest
--- src/tests/java/auth/dev/net/alltests/BaseClassesForTests/BaseTestsFirefox.java
+++ src/tests/java/auth/dev/net/alltests/BaseClassesForTests/BaseTestsFirefox.java
@@ -1,19 +1,17 @@
-package net.ukr.dev.BasesClassesForTests;
+package auth.dev.net.alltests.BaseClassesForTests;
 
 import org.apache.log4j.Logger;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTestsFirefox {
-    final static Logger logger = Logger.getLogger(BaseTests.class);
+    final static Logger logger = Logger.getLogger(BaseTestsFirefox.class);
 
     @BeforeTest
     public static void setup() {
         logger.info("Run browser...");
-//        System.setProperty("selenide.browser", "firefox");
-//        System.setProperty("webdriver.chrome.driver", "D:/AutomationTest/authtest/geckodriver.exe");
-        System.setProperty("selenide.browser", "opera");
-        System.setProperty("webdriver.opera.driver", "D:/AutomationTest/authtest/operadriver.exe");
+        System.setProperty("selenide.browser", "firefox");
+        System.setProperty("webdriver.chrome.driver", "D:/AutomationTest/auth20/src/tests/java/auth/dev/net/resources");
     }
 
     @AfterTest
