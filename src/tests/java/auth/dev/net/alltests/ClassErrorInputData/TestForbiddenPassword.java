a343a36ef319518a4c799967f921a26a556a1a9f
s.kuliesh <s.kuliesh>
2018-07-03 20:55:18 +0300

    update forbiddenPass for correctly working

--- src/tests/java/auth/dev/net/alltests/ClassErrorInputData/TestForbiddenPassword.java
+++ src/tests/java/auth/dev/net/alltests/ClassErrorInputData/TestForbiddenPassword.java
@@ -1,3 +1,4 @@
+//доробити тест з урахування другої сторінки у файлі
 package auth.dev.net.alltests.ClassErrorInputData;
 
 import auth.dev.net.alltests.BaseClassesForTests.BaseTestsChrome;
@@ -14,7 +15,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.refresh;
@@ -27,7 +27,7 @@ public class TestForbiddenPassword extends BaseTestsChrome {
     public Object[][] ForbiddenPasswordUkr (Method method) {
 
         ApachePOIreadHelper excelReader = new ApachePOIreadHelper();
-        File file = new File("//auth20/src/tests/java/auth/dev/net/data_files/test_forbidden_password.xlsx");
+        File file = new File("D:/AutomationTest/auth20/src/tests/java/auth/dev/net/data_files/test_forbidden_password.xlsx");
         excelReader.setExcelFile(String.valueOf(file), "ForbiddenTest");
         List rowsNo = excelReader.getRowContains(method.getName(), 0);
         return excelReader.getTableArray(rowsNo);
@@ -36,7 +36,7 @@ public class TestForbiddenPassword extends BaseTestsChrome {
     @BeforeClass
     public void openPage() throws InterruptedException {
         logger.info("Open page of registration");
-        open("http://accounts.ukr.net/login"); //відкриваємо сторінку входу до поштової скриньки
+        open("http://accounts-new.dev.ukr.net/login"); //відкриваємо сторінку входу до поштової скриньки
         $(By.linkText("Створити скриньку")).click(); //переходимо на сторінку реєстрації поштової скриньки
         Thread.sleep(2000);
         refresh();
@@ -60,7 +60,7 @@ public class TestForbiddenPassword extends BaseTestsChrome {
 
         softAssertion.assertEquals(""+displayedError+"", $(By.cssSelector(".input-default__error.is-size-normal")).getText(), "Пароль '"+inputPassword+"' сприймається системою для Української локалізації");
 
-        refresh();
+        $(By.cssSelector("#id-password")).clear();
         softAssertion.assertAll();
 
     }
