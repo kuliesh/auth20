ff7df0179300ecfb3c34d67ce6634f019e33131d
s.kuliesh <s.kuliesh>
2018-07-04 22:00:19 +0300

    update class for test incorecctly name add class for testing 100 incorectly passwords

--- src/tests/java/auth/dev/net/ClassLimit/TestLimitAuthentOnSS.java
+++ src/tests/java/auth/dev/net/ClassLimit/TestLimitAuthentOnSS.java
@@ -2,17 +2,11 @@ package auth.dev.net.ClassLimit;
 
 import auth.dev.net.alltests.BaseClassesForTests.BaseTestsChrome;
 import com.codeborne.selenide.Selectors;
-import org.apache.xmlbeans.SchemaTypeLoader;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.apache.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.testng.log4testng.Logger;
 
-import java.util.Random;
 
 import static com.codeborne.selenide.Selenide.*;
 import static org.testng.Assert.assertTrue;
@@ -24,11 +18,11 @@ public class TestLimitAuthentOnSS extends BaseTestsChrome {
     @BeforeClass
     public void openPage() throws InterruptedException {
         logger.info("Open page of registration");
-        open("http://accounts.ukr.net/login"); //відкриваємо сторінку входу до поштової скриньки
+        open("http://accounts-new.dev.ukr.net/login"); //відкриваємо сторінку входу до поштової скриньки
 
         //Виконуємо вхід до поштового акаунту
-        $(By.cssSelector("#id-1")).sendKeys("s.kuliesh ");
-        $(By.cssSelector("#id-2")).sendKeys(",fhvfktq!!2018CC");
+        $(By.cssSelector("#id-l")).sendKeys("rest21_final_001");
+        $(By.cssSelector("#id-p")).sendKeys(",fhvfktq!!");
         $(By.cssSelector(".form__submit")).click();
         Thread.sleep(2000);
     }
@@ -36,27 +30,31 @@ public class TestLimitAuthentOnSS extends BaseTestsChrome {
     @Test
     public void InputPassword() throws InterruptedException {
         $(By.cssSelector(".login-button__menu-icon")).click();
-
         $(new Selectors.ByText("Контакти для відновлення")).click();
+        //By iframeSecurity = By.cssSelector(".security__iframe");
 
-        $(By.xpath("//article[1]/ul/div/div/div[2]/button[1]")).click();
+        By iframeContacts = By.cssSelector(".security__iframe");
+        switchTo().frame($(iframeContacts));
+
+        $(new Selectors.ByText("Замінити")).click();
         Thread.sleep(500);
 
-        int i=1;
-        while (i<=100) {
+        int intr=1;
+        while (intr<=101) {
 
-            Random r = new Random();
-            char c = (char)('a' + r.nextInt(32));
-            String inputPass = ",fhvfktq"+c;
+            String inputPass = ",fhvfktq";
 
+            $(By.cssSelector("#id-privilege-password")).clear();
             $(By.cssSelector("#id-privilege-password")).sendKeys(""+inputPass+"");
             $(By.xpath("//section/div[2]/button[1]")).click();
+            logger.info(intr+" раз(а) введено невірний пароль. "+inputPass+"");
 
+            //Thread.sleep(2000);
             assertEquals("Неправильний пароль", $(By.cssSelector(".input-text__error")).getText());
-            logger.info(i+" раз(а) введено невірни пароль.");
-            $(By.cssSelector("#id-privilege-password")).clear();
-            i++;
+            System.out.println(intr+" раз(а) введено невірний пароль. "+inputPass+"");
+            intr++;
         }
+        Thread.sleep(4000);
         assertTrue(($(".form")).isDisplayed());
         logger.info("Тест пройщов успішно");
     }
